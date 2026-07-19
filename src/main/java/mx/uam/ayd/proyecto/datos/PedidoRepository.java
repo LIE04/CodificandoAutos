1d4f2ddf9d9e65f837d6c45be8087f0da24a36c9
Erik Lopez Ibarra <erik.loi1330@gmail.com>
2026-07-18 20:10:12 -0600

    recuperacion de repos

---
+++ src/main/java/mx/uam/ayd/proyecto/datos/PedidoRepository.java
@@ -0,0 +1,15 @@
+package mx.uam.ayd.proyecto.negocio.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import mx.uam.ayd.proyecto.negocio.modelo.Pedido;
+
+/**
+ * Repositorio para la entidad Pedido
+ * Maneja las operaciones de persistencia en la base de datos
+ * 
+ * @author Erik LIE04
+ */
+public interface PedidoRepository extends CrudRepository<Pedido, Integer> {
+    // Spring Boot se encarga de implementar los metodos basicos (save, findById, findAll, etc.)
+    // Si despues necesitamos buscar pedidos por estatus o fecha, lo agregamos aqui
+}
---
+++ src/main/java/mx/uam/ayd/proyecto/datos/ReparacionRepository.java
@@ -0,0 +1,12 @@
+package mx.uam.ayd.proyecto.datos;
+
+import org.springframework.data.repository.CrudRepository;
+import mx.uam.ayd.proyecto.negocio.modelo.Reparacion;
+
+/**
+ * Repositorio para la entidad Reparacion
+ * Maneja las operaciones de persistencia en la base de datos
+ * @author Erik LIE04
+ */
+public interface ReparacionRepository extends CrudRepository<Reparacion, Integer> {
+}
