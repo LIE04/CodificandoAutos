3a1c108040f9e942451c94f41e45c896bc87a031
Angel Yael Morales Andrade <yaelol62@gmail.com>
2026-07-19 21:05:42 -0600

    Corregi un error en repositorio DetallesFalla y en la entidad DetallesFalla

--- src/main/java/mx/uam/ayd/proyecto/datos/DetallesFallaRepository.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/DetallesFallaRepository.java
@@ -3,10 +3,10 @@ package mx.uam.ayd.proyecto.datos;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import mx.uam.ayd.proyecto.negocio.modelo.DetallesFalla;
-import mx.uam.ayd.proyecto.negocio.modelo.Cita;
+import mx.uam.ayd.proyecto.negocio.modelo.Reparacion;
 
 public interface DetallesFallaRepository extends CrudRepository<DetallesFalla, Long> {
 
-    public List<DetallesFalla> findByCita(Cita cita);
+    public List<DetallesFalla> findByReparacion(Reparacion reparacion);
     public List<DetallesFalla> findByEstatus(String estatus);
 }
